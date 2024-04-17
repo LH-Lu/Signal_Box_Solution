5220969d18026f7d54dd2b9dedf5a0fde7cebde4
L Lv <luleheng@live.com>
2024-04-17 12:11:54 +0800

    Updated condition sets for DOWN aspect signals

--- Signal_Box/Main.cpp
+++ Signal_Box/Main.cpp
@@ -16,6 +16,8 @@
 
 int main() {
 
+	// TO DO: Just fill in the condition sets for the down line and debug and test :D
+
 	// Initialise signals. [EDIT THIS SECTION]
 	std::unordered_map<std::string, SigPara> signal;
 
@@ -28,9 +30,9 @@ int main() {
 	signal["R01"] = { false, "T01F, T02F, R02T || T01T, T02F, R04F, Y03F", "T01F, T02F, Y01F || T01T, T02F, Y01F", 20 };
 	signal["R02"] = { false, "T01F, T02F, 0000 || T01F, T02T, 0000",	   "T01F, T02F, R01F || T01F, T02T, 0000", 70 };
 	// DOWN aspect signals
-	signal["Y03"] = { false, "R04T", "0000", -70 };
-	signal["R04"] = { false, "R05T", "Y03F", -60 };
-	signal["R05"] = { false, "0000", "R04F", -10 };
+	signal["Y03"] = { false, "T11F, T22F, R04T || T11T, T22F, R04T",	   "T11F, T22F, 0000 || T11T, T22F, 0000", -70 };
+	signal["R04"] = { false, "T11F, T22F, R05T || T11T, T22F, R01F, Y01F", "T11F, T22F, Y03F || T11T, T22F, Y03F", -60 };
+	signal["R05"] = { false, "T11F, T22F, 0000 || T11F, T22T, 0000",	   "T11F, T22F, R04F || T11F, T22T, 0000", -10 };
 
 	// SWITCH POINT pair 1 (everything has to be in default state in order to change switch points) Note, switch pairs interlocked tgt --> if one is in true state, the other will also be in true state
 	signal["T01"] = { false, "T02F, T22F, Y01F, R01F, R02F, Y03F, R04F, R05F", "Y01F, R01F, R02F, Y03F, R04F, R05F", 30 };
